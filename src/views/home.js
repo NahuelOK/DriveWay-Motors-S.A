92d2ca8c97c720a2fd4c6c7f85306840af1592f9
nahuel jesus moreno <nahuel23009@gmail.com>
2024-08-20 22:08:03 -0300

    Agregue el fondo (A chequear)

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,12 @@
-function Home(){
-    return(
-        <div>
-          <h1>Hola</h1>
+import React from 'react';
+import Fondo from "../imgs/fondo.png";
+
+function Home() {
+    return (
+        <div className='contenedor-fondo'>
+            <img className="fondo" src={Fondo} alt="Fondo" />
         </div>
-    )
+    );
 }
 
 export default Home;
